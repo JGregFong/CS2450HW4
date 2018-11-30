a05e9f99692c9dcdb8b03d6f02fa7b0b3e9acfce
Jonathan <jgregfong@gmail.com>
2018-11-29 16:36:58 -0800

    Added a few imports.

--- CS2450HW4/src/project/Assignment4.java
+++ CS2450HW4/src/project/Assignment4.java
@@ -1,5 +1,34 @@
 package project;
 
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+
+import java.util.Optional;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.FillTransition;
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCombination;
+import javafx.geometry.*;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.StackedBarChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.*;
+
 public class Assignment4 {
 
 	public static void main(String[] args) {
